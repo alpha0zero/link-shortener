20fa22074ba9ab3533881663a3b2e3afaacd1802
mo0zero <mourdazim@gmail.com>
2022-09-28 17:04:34 +0100

    updated the ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,38 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
+
+type Status = 'loading' | 'unloaded' | 'loaded'
 
 const Home: NextPage = () => {
+
+  const [status, setStatus] = useState<Status>('unloaded')
+
   return (
-    <div>
-      <form>
-        <label htmlFor='url'>
-          URL:
+    <div className='vh-100 d-flex align-items-center justify-content-center'>
+      
+      <form className='w-l-50 p-3 border rounded-4'>
+        <h2 className='text-center'> CREATE SHORT LINK </h2>
+        <div>
+          <label htmlFor='url'>
+            URL:
+          </label>
           <input className='form-control' type='text' id='url' name='url'/>
-        </label>
-        <label htmlFor="slug">
+        </div>
+
+        <div>
+          <label htmlFor="slug">
+            SLUG:
+          </label>
           <input className='form-control' type='text' id='slug' name='slug'/>
-        </label>
+        </div>
+
+        <div className='mt-2 text-center'>
+          <button onClick={() => setStatus('loading')} className='btn btn-primary w-100' type='submit'>
+            <span className={`spinner-border spinner-border-sm mx-2 ${status === 'loading' ? 'd-inline-block' : 'd-none'}`}></span>
+            CREATE SHORT LINK 
+          </button>
+        </div>
+
       </form>
     </div>
   )
