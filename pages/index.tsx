43d05f7db5d000d22e4debaddc21974bcd7d9d41
mo0zero <mourdazim@gmail.com>
2022-09-28 16:20:06 +0100

    adding bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,17 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      Home
-    </h1>
+    <div>
+      <form>
+        <label htmlFor='url'>
+          URL:
+          <input className='form-control' type='text' id='url' name='url'/>
+        </label>
+        <label htmlFor="slug">
+          <input className='form-control' type='text' id='slug' name='slug'/>
+        </label>
+      </form>
+    </div>
   )
 }
 
