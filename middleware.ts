7b653eda837e2fa659ed211cab7a5b051eecf7f0
mo0zero <mourdazim@gmail.com>
2022-09-29 15:33:02 +0100

    added middleware for redirection

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { Link } from '@prisma/client'
+
+
+export async function middleware(request: NextRequest) {
+
+    if (request.nextUrl.pathname.startsWith('/api')) {
+
+        console.log('we\'re returning');
+        return
+        
+    }
+
+    if(request.nextUrl.pathname !== '/') {
+
+        const slug = request.nextUrl.pathname.split('/').pop()
+        console.log(slug);
+            
+        const link: Link = await (await fetch(`${request.nextUrl.origin}/api/${slug}`)).json()
+        
+        return NextResponse.redirect(link.url)
+    }
+
+
+    
+}
+
