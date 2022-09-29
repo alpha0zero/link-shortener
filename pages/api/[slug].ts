7f10fac05afc00ae2860ddead5d08af4fd49cadc
mo0zero <mourdazim@gmail.com>
2022-09-29 14:46:10 +0100

    added the slug route

---
+++ pages/api/[slug].ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { prisma } from "../../db/prismaClient"
+
+const hundler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { slug } = req.query
+    console.log(req.query);
+    
+
+    if (slug) {
+        try{
+            const data = await prisma?.link.findFirst({
+                where: {
+                    slug: {
+                        equals: `${slug}`,
+                    }
+                }
+            })
+            res.status(201).json(data)
+        }catch(_){
+            res.end('couldn\'t get the data')
+        }
+    }
+}
+
+export default hundler
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,12 +16,13 @@ const Home: NextPage = () => {
   const hundleSubmit = async (e: FormEvent<EventTarget>) => {
 
     e.preventDefault()
+
     const { data } = await axios.post('/api/link',
       link
     )
-
+    
     console.table(data)
-    if(data && data !== undefined) setStatus('loaded')
+    if(data) setStatus('loaded')
   }
 
   const [status, setStatus] = useState<Status>('unloaded')
@@ -33,20 +34,20 @@ const Home: NextPage = () => {
   return (
     <div className='vh-100 d-flex align-items-center justify-content-center'>
       
-      <form onSubmit={hundleSubmit} className='w-l-50 p-3 border rounded-4'>
+      <form onSubmit={hundleSubmit} className='w-75 px-3 py-5 border rounded-4'>
         <h2 className='text-center'> CREATE SHORT LINK </h2>
         <div>
           <label htmlFor='url'>
             URL:
           </label>
-          <input onChange={(e) => setLink({...link, url: e.target.value})} className='form-control' type='text' id='url' name='url'/>
+          <input onChange={(e) => setLink({...link, url: e.target.value})} className='form-control' type='text' id='url' name='url' required/>
         </div>
 
         <div>
           <label htmlFor="slug">
             SLUG:
           </label>
-          <input onChange={(e) => setLink({...link, slug: e.target.value})} className='form-control' type='text' id='slug' name='slug'/>
+          <input onChange={(e) => setLink({...link, slug: e.target.value})} className='form-control' type='text' id='slug' name='slug' required/>
         </div>
 
         <div className='mt-2 text-center'>
